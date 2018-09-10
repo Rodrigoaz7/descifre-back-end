5d0c12c6e3c42b323116f96957230d7d2c0dc2a8
rodrigo <rodrigo.aze7@gmail.com>
2018-09-10 10:56:07 -0300

    Controllers e Rotas para deletar e listar rodadas

--- src/routes/rodada.js
+++ src/routes/rodada.js
@@ -1,8 +1,12 @@
 const permissao = require('../middlewares/permissoes');
 const variables = require('../../config/variables');
 const controllerCriarRodada = require('../controllers/administrador/rodadas/criarRodada');
+const controllerRemoverRodada = require('../controllers/administrador/rodadas/deletarRodada');
+const controllerRodadas = require('../controllers/administrador/rodadas/listarRodadas')
 
 module.exports = (application) => {
     application.post(`${variables.base}/administrador/rodadas`, permissao.administrador, (req, res) => {controllerCriarRodada.cadastrarRodada(req, res)});
+    application.delete(`${variables.base}/administrador/rodadas`, permissao.administrador, (req, res) => {controllerRemoverRodada.removerRodada(req, res)});
+    application.get(`${variables.base}/administrador/rodadas/:token`, permissao.administrador, (req, res) => {controllerRodadas.listarRodadas(req, res)});
 
 };
