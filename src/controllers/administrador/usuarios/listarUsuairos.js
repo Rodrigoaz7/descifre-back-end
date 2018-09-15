2958a906fadf98aa454e03b8202860c4f4ca9588
rodrigo <rodrigo.aze7@gmail.com>
2018-09-15 14:27:14 -0300

    Match Sorter na busca de usuarios

--- src/controllers/administrador/usuarios/listarUsuairos.js
+++ src/controllers/administrador/usuarios/listarUsuairos.js
@@ -3,12 +3,22 @@
 */
 const mongoose = require('mongoose');
 const Usuario = mongoose.model('Usuario');
+const Pessoa = mongoose.model('Pessoa');
 const httpCodes = require('../../../util/httpCodes');
 const responses = require('../../../util/responses');
+const matchSorter = require('match-sorter');
 
 exports.listarUsuarios = async (req, res) => {
     const limite = parseInt(req.params.limite);
-    const usuarios = await Usuario.find({}).populate('pessoa').limit(limite).exec();
+    const filtro = req.query.filtro;
+    var usuarios = [];
+    if(filtro){
+    	const pessoas = await Pessoa.find({});
+    	const resultado = matchSorter(pessoas, filtro, {keys: ['nome', 'email']});
+    	usuarios = await Usuario.find({pessoa: resultado}).populate('pessoa').limit(limite).exec();
+    } else {
+    	usuarios = await Usuario.find({}).populate('pessoa').limit(limite).exec();
+    }
     console.log(parseInt(httpCodes.getValue("OK")))
     return res.status(parseInt(httpCodes.getValue("OK"))).json({status: true, msg: responses.getValue("usuariosListados"), usuarios: usuarios});
 };
