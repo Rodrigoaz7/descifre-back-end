145475fd9179febfa01f3efa34f1427fc9553c94
Marcus Dantas <mpsdantas15@gmail.com>
2018-10-12 18:39:55 -0300

    teste de cors

--- config/app.js
+++ config/app.js
@@ -59,6 +59,10 @@ mongoose.connection.on('error',err => {
 
 app.use(cors());
 app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'descifre.com');
+	res.header('Access-Control-Allow-Origin', 'www.descifre.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
