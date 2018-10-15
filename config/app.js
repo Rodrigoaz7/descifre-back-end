bf070561dce19016860028692bb3cd09d76ce358
Marcus Dantas <mpsdantas15@gmail.com>
2018-10-15 11:58:55 -0300

    Ajuste cors.

--- config/app.js
+++ config/app.js
@@ -57,7 +57,31 @@ mongoose.connection.on('error',err => {
 	console.log(`🙅 🚫 → ${err.message}`);
 });
 
+
+app.use((req, res, next)=>{
+	if (req.method === 'OPTIONS') {
+		console.log('!OPTIONS');
+		var headers = {};
+		// IE8 does not allow domains to be specified, just the *
+		// headers["Access-Control-Allow-Origin"] = req.headers.origin;
+		headers["Access-Control-Allow-Origin"] = "*";
+		headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+		headers["Access-Control-Allow-Credentials"] = false;
+		headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+		headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+		res.writeHead(200, headers);
+		res.end();
+	}else{
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+		res.setHeader("Access-Control-Allow-Headers", "content-type");
+		res.setHeader("Access-Control-Allow-Credentials", true);
+	}
+    next();
+});
+
 app.use(cors());
+
 
 /* efetua o autoload das rotas, dos models e dos controllers para o objeto app */
 consign().include('src/models')
