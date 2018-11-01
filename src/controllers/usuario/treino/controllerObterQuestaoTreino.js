2abf4c7b6a7a26825d12a4b26842ac58a11cda40
Marcus Dantas <mpsdantas15@gmail.com>
2018-11-01 13:50:35 -0300

    Controller obter questão.

---
+++ src/controllers/usuario/treino/controllerObterQuestaoTreino.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const ObjectID = require('mongodb').ObjectID;
+const httpCodes = require('../../../util/httpCodes');
+const Treino = mongoose.model('Treino');
+const Questao = mongoose.model('Questao');
+exports.enviarQuestao = async (req, res) =>{
+    if(!req.params.idTreino) return res.status(httpCodes.getValue('NotFound')).json({status:false, msg:"Você precisa enviar um id do treino."});
+
+    if(!req.params.idUsuario) return res.status(httpCodes.getValue('NotFound')).json({status:false, msg:"Você precisa enviar um id do usuário."});
+
+    let buscarTreino;
+
+    try {
+        buscarTreino = await Treino.findOne({_id: req.params.idTreino});
+    } catch (error) {
+        return res.status(httpCodes.getValue('ServerErro')).json({status:false, msg:"Erro de processamento interno: id do treino não é válido."});
+    } 
+    
+    if(String(buscarTreino.usuario)!==String(req.params.idUsuario)) return res.status(httpCodes.getValue('ServerErro')).json({status:false, msg:"Erro de processamento interno: id do usuário não está associado ao treino."});
+
+    let qntdQuestoes = await Questao.count();
+    
+    let randomNumber = Math.floor(Math.random() * qntdQuestoes);
+
+    let questaoSelecionada = await Questao.findOne().populate('categoria').skip(randomNumber).exec();
+
+    delete questaoSelecionada['corretaTexto'];
+    delete questaoSelecionada['correta'];
+    delete questaoSelecionada['pontuacao'];
+
+    res.status(httpCodes.getValue('OK')).json({status: true, msg: "Questão enviada com sucesso.", questao: questaoSelecionada});
+
+};
--- src/routes/treino.js
+++ src/routes/treino.js
@@ -2,7 +2,12 @@ const permissao = require('../middlewares/permissoes');
 const variables = require('../../config/variables');
 const controllerGetTreino = require('../controllers/usuario/treino/controllerGetTreino');
 const controllerEntrarTreino = require('../controllers/usuario/treino/controllerEntrarTreino');
+const controllerObterQuestao = require('../controllers/usuario/treino/controllerObterQuestaoTreino');
+
 module.exports = (application) => {
     application.get(`${variables.base}/usuario/treino/:idUsuario/:token`, permissao.usuario, (req, res) => {controllerGetTreino.getTreino(req, res)});
+
+    application.get(`${variables.base}/usuario/treino/questao/:idTreino/:idUsuario/:token`, permissao.usuario, (req, res) => {controllerObterQuestao.enviarQuestao(req, res)});
+
     application.post(`${variables.base}/usuario/treino`, permissao.usuario, (req, res) => {controllerEntrarTreino.entrarNoTreino(req, res)});
 };
