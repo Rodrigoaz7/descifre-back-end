6e58c58c036372341ad3ec176eb18f3bc07919f2
Marcus Dantas <mpsdantas15@gmail.com>
2018-11-08 05:05:19 -0300

    Commit for produção.

--- config/variables.js
+++ config/variables.js
@@ -1,7 +1,7 @@
 const variables = {
     secret: "tokensLegaisDescifre",
     base: '/api/v1',
-    pagseguroHost: 'ws.sandbox.pagseguro.uol.com.br',
+    pagseguroHost: 'ws.pagseguro.uol.com.br',
     tokenPagseguro: 'C67E7939A1454B9E9515A0774F1C4412',
     emailPagseguro: 'empresadescifre@gmail.com',
     tokenEmail: '5bb2a45279bf281121646306',
