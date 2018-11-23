995c6e9eeaf858436aeb9f4aa2380576ee44c8ec
Marcus Dantas <mpsdantas15@gmail.com>
2018-11-23 10:41:29 -0300

    Ajuste categorias questões em produção.

--- src/controllers/usuario/questao/controladorQuestaoAleatoria.js
+++ src/controllers/usuario/questao/controladorQuestaoAleatoria.js
@@ -10,7 +10,7 @@ const Quiz = mongoose.model('Quiz');
 const Questao = mongoose.model('Questao');
 
 const httpCodes = require('../../../util/httpCodes');
-
+const variaveis = require('../../../../config/variables');
 
 exports.gerarQuestaoAleatoria = async (req, res) => {
     // Salvando em variaveis o id do usuario e rodada.
@@ -20,9 +20,9 @@ exports.gerarQuestaoAleatoria = async (req, res) => {
     // Verificar se existe um quiz associado a rodada e ao usuário.
 
     let buscaQuiz = await Quiz.findOne({_id: new ObjectID(idQuiz), idUsuario: idUsuario});
-    
+
     let dataAbertura = new Date();
-    
+
     if(buscaQuiz!==null){
         let buscaRodada = await Rodada.findOne({_id: new ObjectID(buscaQuiz.idRodada)});
         let dataAberturaQuiz = new Date(buscaQuiz.dataAbertura);
@@ -30,15 +30,37 @@ exports.gerarQuestaoAleatoria = async (req, res) => {
         let dataJogada = new Date(dataAberturaQuiz.getTime()+parseInt(buscaRodada.duracao)*60000);
         if(dataAbertura.getTime() > dataFinalizacaoQuiz.getTime() || dataAbertura.getTime()>dataJogada.getTime()) return res.status(httpCodes.getValue('NaoAutorizado')).json({status:true, finalizado:true, msg:"Você já jogou esse quiz e o tempo foi esgotado."});
     }
+
+    let arrayCategorias = ['5bbec3db9be1c212b3de8855', '5bbec3db9be1c212b3de8858',
+        '5bbec3db9be1c212b3de8859', '5bbec3db9be1c212b3de8865',
+        '5bbec3db9be1c212b3de8866', '5bbec3dc9be1c212b3de891f', 
+        '5bbec3dc9be1c212b3de8925', '5bbec3dc9be1c212b3de8930',
+        '5bbec3dc9be1c212b3de8932', '5bbec3dc9be1c212b3de8933',
+        '5bbec3dc9be1c212b3de8934', '5bbec3dc9be1c212b3de8937',
+        '5bbec3dc9be1c212b3de8a0b', '5bbec3dc9be1c212b3de8a0d',
+        '5bbec3dc9be1c212b3de8a10', '5bbec3dc9be1c212b3de89fe', 
+    ];
     
-    let qntdQuestoes = await Questao.count();
+    let condicionalPesquisa = {};
+
+    if(variaveis.ambiente!='dev'){
+        condicionalPesquisa = {
+            'categoria': {
+                $in: arrayCategorias
+            }
+        };
+    }
+    console.log(condicionalPesquisa)
+    let qntdQuestoes = await Questao.count(condicionalPesquisa);
     
     let randomNumber = Math.floor(Math.random() * qntdQuestoes);
 
-    let questaoSelecionada = await Questao.findOne().populate('categoria').skip(randomNumber).exec();
+    let questaoSelecionada = await Questao.findOne(condicionalPesquisa).populate('categoria').skip(randomNumber).exec();
+
     delete questaoSelecionada['corretaTexto'];
     delete questaoSelecionada['correta'];
     delete questaoSelecionada['pontuacao'];
+
     while(true){
         if(!buscaQuiz.jogadas.find(x=> x.questao==questaoSelecionada._id)){
             break;
@@ -48,7 +70,7 @@ exports.gerarQuestaoAleatoria = async (req, res) => {
         delete questaoSelecionada['correta'];
         delete questaoSelecionada['pontuacao'];
     }
-    
+
     return res.status(httpCodes.getValue('OK')).json({status: true, finalizado:false, questao: questaoSelecionada});
 
 
