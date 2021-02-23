ff8ae699d174c30573aa7badfdcaba4ae07d66dd
Rodrigo Azevedo <rodrigo.aze7@gmail.com>
2021-02-23 15:56:57 -0300

    .

--- config/app.js
+++ config/app.js
@@ -31,7 +31,7 @@ app.use(function(req, res, next){
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, application/json");
     res.setHeader("Access-Control-Allow-Credentials", true);
 
     next();
